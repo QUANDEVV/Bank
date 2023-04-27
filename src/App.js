5443c5176905ac7a1ee8b26159548e98407399a4
QUANDEVV <vercelploy@gmail.com>
2023-04-27 13:58:06 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Form from './components/Form';
 
 const App = () => {
   const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
   const deleteTransaction = (id) => {
     setTransactions(transactions.filter(transaction => transaction.id !== id));
@@ -15,14 +18,41 @@ const App = () => {
   useEffect(() => {
     fetch(`http://localhost:5000/transaction/`)
       .then(response => response.json())
-      .then(data => setTransactions(data));
+      .then(data => {
+        setTransactions(data);
+        setFilteredTransactions(data);
+      });
   }, []);
+
+  useEffect(() => {
+    if (selectedCategory === 'All') {
+      setFilteredTransactions(transactions);
+    } else {
+      setFilteredTransactions(transactions.filter(transaction => transaction.category === selectedCategory));
+    }
+  }, [transactions, selectedCategory]);
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
 
   return (
     <div className='App'>
       <Header />
       <Form />
-      <Table transactions={transactions} onDelete={deleteTransaction} />
+      <div className="filter-container">
+        <label>Filter by Category:</label>
+        <select value={selectedCategory} onChange={handleCategoryChange}>
+          <option value="All">All</option>
+          <option value="Food">Food</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Income">Income</option>
+          <option value="Transportation">Transportation</option>
+          <option value="Gift">Gift</option>
+          <option value="Fashion">Fashion</option>
+        </select>
+      </div>
+      <Table transactions={filteredTransactions} searchTerm={searchTerm} onDelete={deleteTransaction} />
       <Footer />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -6,14 +6,11 @@ const Form = ({ addTransaction }) => {
   const [category, setCategory] = useState('');
   const [amount, setAmount] = useState('');
 
-
-
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const transaction = { date, description, category, amount };
 
-    fetch(`  http://localhost:5000/transaction/`, {
+    fetch(`http://localhost:5000/transaction/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -28,53 +25,48 @@ const Form = ({ addTransaction }) => {
       .catch(error => console.error(error));
   };
 
-
-
-
-
-
   return (
     <div className="container">
-    <form onSubmit={handleSubmit}>
-      <label>
-        Date:
-        <input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Description:
-        <input
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Category:
-        <input
-          type="text"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Amount:
-        <input
-          type="number"
-          step="0.01"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Add Transaction</button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Date:
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Description:
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Category:
+          <input
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Amount:
+          <input
+            type="number"
+            step="0.01"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Add Transaction</button>
+      </form>
     </div>
   );
 };
